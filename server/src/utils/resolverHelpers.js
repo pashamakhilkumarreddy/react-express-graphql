d5dac7d836abedc282e4b64099b0a0b2ecbdb623
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2020-06-17 12:45:57 +0530

    add dataloader

--- server/src/utils/resolverHelpers.js
+++ server/src/utils/resolverHelpers.js
@@ -1,26 +1,22 @@
 /*
-  eslint-disable no-underscore-dangle
+  eslint-disable no-underscore-dangle, no-use-before-define
  */
+const DataLoader = require('dataloader');
+
 const {
   User,
   Event,
 } = require('../models');
 
+const eventLoader = new DataLoader((eventIds) => getEvents(eventIds));
+
+const userLoader = new DataLoader((userIds) => User.find({
+  _id: {
+    $in: userIds,
+  },
+}));
+
 const convertDateToString = (date) => new Date(date).toISOString();
-
-const transformEvent = (event) => ({
-  ...event._doc,
-  date: convertDateToString(event._doc.date),
-  creator: getUser(event._doc.creator), // eslint-disable-line no-use-before-define
-});
-
-const transformBooking = (booking) => ({
-  ...booking._doc,
-  user: getUser(booking._doc.user), // eslint-disable-line no-use-before-define
-  event: getEventDetails(booking._doc.event), // eslint-disable-line no-use-before-define
-  createdAt: convertDateToString(booking._doc.createdAt),
-  updatedAt: convertDateToString(booking._doc.updatedAt),
-});
 
 async function getEvents(eventIds) {
   try {
@@ -29,20 +25,7 @@ async function getEvents(eventIds) {
         $in: eventIds,
       },
     });
-    return events.map((event) => (transformEvent(event)));
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-}
-
-async function getUser(userID) {
-  try {
-    const user = await User.findById(userID);
-    return {
-      ...user._doc,
-      createdEvents: getEvents(user._doc.createdEvents),
-    };
+    return events.map((event) => transformEvent(event));
   } catch (err) {
     console.error(err);
     throw err;
@@ -51,13 +34,40 @@ async function getUser(userID) {
 
 async function getEventDetails(eventId) {
   try {
-    const event = await Event.findById(eventId);
+    const event = await eventLoader.load(eventId.toString());
     return transformEvent(event);
   } catch (err) {
     console.error(err);
     throw err;
   }
 }
+
+async function getUser(userID) {
+  try {
+    const user = await userLoader.load(userID.toString());
+    return {
+      ...user._doc,
+      createdEvents: eventLoader.load(user._doc.createdEvents),
+    };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+const transformEvent = (event) => ({
+  ...event._doc,
+  date: convertDateToString(event._doc.date),
+  creator: getUser(event._doc.creator),
+});
+
+const transformBooking = (booking) => ({
+  ...booking._doc,
+  user: getUser(booking._doc.user),
+  event: getEventDetails(booking._doc.event),
+  createdAt: convertDateToString(booking._doc.createdAt),
+  updatedAt: convertDateToString(booking._doc.updatedAt),
+});
 
 module.exports = {
   convertDateToString,
